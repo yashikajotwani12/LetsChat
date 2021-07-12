8e2e8be249097c200cee470e63346af800cc25ed
Yashika <yashikajotwani12@gmail.com>
2021-07-12 21:44:12 +0530

    done

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from chat.models import Room, Message
 
 # Create your views here.
+def home(request):
+    return render(request,'home.html' )
+
+def room(request,room):
+    username = request.GET.get('username')
+    room_details = Room.objects.get(name= room)
+    return render(request,'room.html',{
+        'username':username,
+        'room':room,
+        'room_details':room_details,
+    } )
+
+def checkview(request):
+   
+
+    room = request.POST['room_name']
+    username = request.POST['username']
+
+    if Room.objects.filter(name = room).exists():
+        return redirect('/'+room+'/?username='+username)
+    else:
+        new_room = Room.objects.create(name= room)
+        new_room.save()
+        return redirect('/'+room+'/?username='+username)
+
